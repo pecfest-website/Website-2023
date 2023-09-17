ce2c30a6c5a011ba7ad2a6919f269e1b3c23c496
dakshgupta2002 <dakshgupta2002@gmail.com>
2023-09-17 22:48:26 +0530

    Event List and Details page

--- src/components/events/eventCard.tsx
+++ src/components/events/eventCard.tsx
@@ -1,7 +1,7 @@
 // components/EventCard.tsx
-import React from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
-
+import React, { useState } from 'react';
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import ModalContainer from './ModalContainer/ModalContainer.js' 
 interface Event {
   id: number;
   title: string;
@@ -14,22 +14,72 @@ interface EventCardProps {
   data: Event;
 }
 
+function ItemSepWithDot(items: string[]) {
+  var f = items?.length;
+  f -= 1;
+  const jsx_items = [];
+  for (let i = 0; i < items?.length; i++) {
+    jsx_items.push(
+      <div key={i} className="genre-list">
+        {" "}
+        {items[i]}
+        <span className={f-- !== 0 ? `center-dot` : ``}></span>
+      </div>
+    );
+  }
+  return jsx_items;
+}
+
 const EventCard: React.FC<EventCardProps> = ({ data }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Card className="bg-blue-200 border-blue-300 rounded-lg shadow-md">
-      <CardContent>
-        <Typography variant="h6" className="text-blue-800">
-          {data.title}
-        </Typography>
-        <Typography variant="body2" className="text-blue-600">
-          {data.date}
-        </Typography>
-        <Typography variant="body2" className="text-blue-600">
-          {data.location}
-        </Typography>
-        {/* Add more event details as needed */}
-      </CardContent>
-    </Card>
+    <div className="movieCardContainer">
+      <ModalContainer
+        isOpen={open}
+        close={() => setOpen(false)}
+        style={{ backgroundColor: "rgb(47 47 47 / 98%)" }}
+        className=''
+        padding={0}
+      >
+        Details of the event and registration
+      </ModalContainer>
+
+      <div
+        className="posterContainer"
+        style={{
+          backgroundImage: `url()`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <h3>1200 registered </h3>
+      </div>
+
+      <div className="movieData">
+        <div className="movieDetailContainer">
+          <div className="movieName">{'title'}</div>
+          <div className="genre">{ItemSepWithDot(['genres'])}</div>
+
+          <div className="releaseDate">
+            <div className="content">{'release_data'}</div>
+            <Button
+              variant="contained"
+              sx={{
+                margin: 0,
+                padding: 0,
+                color: "white",
+                fontWeight: "bold",
+                backgroundColor: "#EB5353",
+              }}
+              onClick={(e) => setOpen(true)}
+            >
+              More
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
