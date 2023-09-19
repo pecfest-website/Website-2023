ec4e2688758db2d52b764a1de0f69c571ee804e5
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-09-20 01:39:17 +0530

    Bug fix : Underwater buildings loaded

--- src/components/landing/screens/Underwater.tsx
+++ src/components/landing/screens/Underwater.tsx
@@ -1,8 +1,5 @@
 import { Layers } from "@/components/landing/util/layers";
-import hill3 from "../assets/underwater/hill last.png";
-import hill2 from "../assets/underwater/hill middle.png";
-import hill1 from "../assets/underwater/hill front.png";
-import buildings from "../assets/underwater/buildings.png";
+import underwater from "../assets/underwater2.png";
 import statisticsText from "../assets/statistics.png";
 
 function Underwater() {
@@ -18,6 +15,7 @@ function Underwater() {
             <Layers pos={[0, -200, -40]} args={[150, 75]} img={hill2.src} />
             <Layers pos={[0, -200, -40]} args={[150, 75]} img={hill1.src} /> */}
 
+            <Layers pos={[0, -200, -40]} args={[150, 77]} img={underwater.src} /> 
             {sizes.width > 960 ? (
                 <Layers
                     pos={[0, -170, -30]}
