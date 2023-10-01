9d772a699ac73eaa28dafbc0b128c3b1e866b73e
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-10-02 01:50:29 +0530

    hover box implemented!

--- src/components/landing/util/HoverBox.tsx
+++ src/components/landing/util/HoverBox.tsx
@@ -1,14 +1,27 @@
+import { useLoader } from "@react-three/fiber";
 import React from "react";
+import * as THREE from "three";
 
 function HoverBox({
     title,
     description,
+    img,
+    pos,
+    args,
 }: {
     title: string;
     description: string;
+    img: string;
+    pos: any;
+    args: any;
 }) {
-    return <>
-    </>;
+    const texture = useLoader(THREE.TextureLoader as any, img);
+    return (
+        <mesh position={pos}>
+            <planeGeometry attach="geometry" args={args} />
+            <meshBasicMaterial attach="material" map={texture} transparent />
+        </mesh>
+    );
 }
 
 export default HoverBox;
