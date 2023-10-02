ec54ecad1d5fcc2ea181f2072abaa7cff525edf5
ugpec79 <ujjawalguptapec@gmail.com>
2023-10-03 03:24:35 +0530

    Sponsors Page added

---
+++ src/data/sponsor.ts
@@ -0,0 +1,164 @@
+export const sponsors = {
+    "Title Sponsor": [{
+            "Name": "Trident",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Driven By": [{
+            "Name": "Sparx",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Co Sponsor": [{
+            "Name": "Golmedal",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Associate Partner":[{
+            "Name": "JLPL",
+            "Logo": "/assets/logos/logo.png"
+        },   
+        {
+            "Name": "Quark City",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Jubilee Group",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Corporate Partner":[{
+            "Name": "Venus Education",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Hit Bulls Eye",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Xeam",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "IBS",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Trishla City",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Zoom Car",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Union Bank",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "HLP Galleria",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "JSW Steel",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Canara",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Merchandise Partner":[{
+            "Name": "Bonkers Corner",
+            "Logo": "/assets/logos/logo.png"
+    }],
+    "Engagement Partner":[{
+            "Name": "100 things in Ldh",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Chandigarh Bytes",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Experience Partner":[{
+            "Name": "Tinder",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Official Hydration Partner": [{
+            "Name": "Jal",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Official Snack Partner": [{
+            "Name": "Crax",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Official Hospitality Partner": [{
+        "Name": "Lalit Chnadigarh",
+        "Logo": "/assets/logos/logo.png"
+    }
+  ],
+    "Photography Partner": [{
+            "Name": "Instax",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Official Payment Partner": [{
+            "Name": "SBI",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Event Sponsors":[{
+            "Name": "Canva",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Crimsoune",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Roadies Koffeehouz",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Ikarus 3D",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ],
+    "Food Partner": [{
+            "Name": "Le Burger",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Awadhi Central",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Baker's In",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Birinj",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Hangry",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Peggy",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Chaat Current",
+            "Logo": "/assets/logos/logo.png"
+        },
+        {
+            "Name": "Tandoori Zaika",
+            "Logo": "/assets/logos/logo.png"
+        }
+    ]
+  }
