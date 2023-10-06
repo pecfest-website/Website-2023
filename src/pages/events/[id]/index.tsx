6fbf4a1d6c8a3f0b8bf9539a9d991374ded5affb
Ishwarendra Jha <ishwarendra27@gmail.com>
2023-10-06 23:08:07 +0530

    form made but formValue need fix

--- src/pages/events/[id]/index.tsx
+++ src/pages/events/[id]/index.tsx
@@ -1,5 +1,5 @@
 import EventCard from "@/components/events/eventCard";
-import { Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, Grid, TextField } from "@mui/material";
+import { Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, Grid, Stack, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import styles from "@/styles/Events/eventDetails.module.css";
 import PageLayout from "@/components/layout/PageLayout";
@@ -33,18 +33,17 @@ function EventDetails({ event }: EventDetailsProps) {
 
   const [open, setOpen] = useState(false);
 
-  const [teamSize, setTeamSize] = useState(1);
-  const [teamName, setTeamName] = useState<string>();
+  const [teamSize, setTeamSize] = useState<number>(1);
+  const [teamName, setTeamName] = useState<string>('');
   const [formValues, setFormValues] = useState<Registrant[]>([defaultRegistrantObj]);
+  const [error, setError] = useState(false);
 
-  const handleTeamSizeChange = (event: any) => {
-    let newTeamSize = event.target.value;
+  const handleTeamSizeChange = (e: any) => {
+    let newTeamSize = e.target.value;
+    const re = /[0-9]+/g;
+    if (!(newTeamSize === '') && !re.test(newTeamSize)) return;
+
     setTeamSize(newTeamSize);
-
-    if (newTeamSize < event.minTeamSize || newTeamSize > event.maxTeamSize) {
-      return;
-    }
-
     let registrantDetails = [...formValues];
 
     while (registrantDetails.length > newTeamSize) {
@@ -63,23 +62,34 @@ function EventDetails({ event }: EventDetailsProps) {
   }
 
   const handleSubmit = () => {
-    console.log(formValues)
     for (let formValue of formValues) {
-      if (formValue.name?.length === 0
+      if (formValue.name.length === 0
         || !formValue.phoneNumber
         || formValue.phoneNumber === 0
         || formValue.userId?.length === 0) {
-
-        console.log(formValue)
+        setError(true);
+        setTimeout(() => {
+          setError(false);
+        }, 3000);
         return;
       }
     }
+
+    if (event.type == 'Team' && (teamName?.length === 0 || (teamSize ?? 0) > event.maxTeamSize || (teamSize ?? 0) < event.minTeamSize)) {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
+      return;
+    }
+
+    setError(false);
     const registrantData = {
       teamName: teamName,
       teamSize: teamSize,
       usersData: formValues
     }
-
+    
     console.log(registrantData);
     formValues.fill(defaultRegistrantObj);
     setTeamSize(1);
@@ -145,6 +155,105 @@ function EventDetails({ event }: EventDetailsProps) {
 
   return (
     <PageLayout title={`${event.name} | PECFEST'23`} darkHeader>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+          <DialogContentText>
+            Fill the details below to register for the event!
+          </DialogContentText>
+          <form onSubmit={handleSubmit}>
+
+            {
+              event.type === 'Team' && (
+                <div>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Team Name"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    required
+                    value={teamName}
+                    onChange={(event: any) => { setTeamName(event.target.value); }}
+                  />
+
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Team Size"
+                    type="number"
+                    fullWidth
+                    variant="standard"
+                    required={true}
+                    value={teamSize}
+                    onChange={handleTeamSizeChange}
+                    error={!(!teamSize || (teamSize >= event.minTeamSize && teamSize <= event.maxTeamSize))}
+                    helperText={`Team size should be between ${event.minTeamSize} and ${event.maxTeamSize}`}
+                    InputProps={{ inputProps: { min: event.minTeamSize, max: event.maxTeamSize } }}
+                  />
+                </div>
+              )}
+
+            {
+              formValues.map((_, id) => (
+                <div key={id} style={{ borderBottom: '1px solid orange', padding: '10px' }}>
+                  <TextField
+                    key={`person-${id}-id`}
+                    name={`person-${id}-id`}
+                    variant='outlined'
+                    label={`Person-${id + 1} Name`}
+                    sx={{ my: 1 }}
+                    fullWidth required
+                    value={formValues[id].name}
+                    onChange={(e: any) => {
+                      let newFormValues = [...formValues];
+                      newFormValues[id].name = e.target.value;
+                      setFormValues(newFormValues);
+                    }}
+                  />
+                  <TextField
+                    variant='outlined'
+                    key={`person-${id}-userId`}
+                    name={`person-${id}-userId`}
+                    label={`Person-${id + 1} UserId`}
+                    sx={{ my: 1 }}
+                    fullWidth required
+                    value={formValues[id].userId}
+                    onChange={(e: any) => {
+                      let newFormValues = [...formValues];
+                      newFormValues[id].userId = e.target.value;
+                      setFormValues(newFormValues);
+                    }}
+                  />
+                  <TextField
+                    variant='outlined'
+                    key={`person-${id}-phoneNumber`}
+                    name={`person-${id}-phoneNumber`}
+                    label={`Person-${id + 1} PhoneNumber`}
+                    sx={{ my: 1 }}
+                    type='number'
+                    fullWidth required
+                    value={formValues[id].phoneNumber}
+                    onChange={(e: any) => {
+                      let newFormValues = [...formValues];
+                      newFormValues[id].phoneNumber = e.target.value;
+                      setFormValues(newFormValues);
+                    }}
+                  />
+                </div>
+              ))
+            }
+            {error && (
+              <span className={styles.errorText}>❌ One or more fields incorrectly filled!</span>
+            )}
+            <div style={{display: 'flex', justifyContent: "center", marginTop: '10px'}}>
+              <Button onClick={handleClose} variant="contained" sx={{marginRight:'10px'}}>Cancel</Button>
+              <Button onClick={handleSubmit} type='submit' variant="contained">Register</Button>
+            </div>
+
+          </form>
+        </DialogContent>
+      </Dialog>
       <section className={styles.background}>
         <main className={`${styles.main}`}>
           <div className={`${styles.details} glassmorphism-light`}>
@@ -197,7 +306,7 @@ function EventDetails({ event }: EventDetailsProps) {
               })}
             </div>
 
-            <Button variant="contained">Regsiter</Button>
+            <Button variant="contained" onClick={() => { setOpen(true) }}>Register</Button>
             <hr className={styles.line} />
 
             <div className={styles.description}>
