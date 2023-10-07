3cc3af9cbd5b01c67014ce9c2d6442895d4800a1
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-10-07 15:30:45 +0530

    gallery fetching from firebase

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -5,14 +5,30 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import IMAGES from "@/data/gallery";
 import styles from "@/styles/Gallery/gallery.module.css";
+import { GetServerSidePropsContext } from "next";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { db } from "@/serverless/firebase";
 
-const slides = IMAGES.map(({ src, width, height }) => ({
-    src: src,
-    width: 800,
-    height: 800,
-}));
+interface ImageObject {
+    src: string;
+    original: string;
+    width: number;
+    height: number;
+    caption: string;
+}
 
-function GalleryView() {
+interface SlidesObject {
+    src: string;
+    height: number;
+    width: number;
+}
+
+interface Props {
+    img: ImageObject[];
+    slides: SlidesObject[];
+}
+
+function GalleryView({ img, slides }: Props) {
     const [index, setIndex] = useState(-1);
     const handleClick = (index: number, _: any) => setIndex(index);
 
@@ -20,7 +36,7 @@ function GalleryView() {
         <PageLayout title="Gallery | Pecfest'23">
             <h2 className={styles.heading}>Past Events and Competition</h2>
             <Gallery
-                images={IMAGES}
+                images={img}
                 enableImageSelection={false}
                 onClick={handleClick}
                 rowHeight={300}
@@ -36,3 +52,43 @@ function GalleryView() {
 }
 
 export default GalleryView;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const docRef = doc(db, "gallery/images");
+    const imagesSnapshot = await getDoc(docRef);
+
+    if (imagesSnapshot.exists()) {
+        const imageUrls = imagesSnapshot.data()["images"] as string[];
+
+        const imgs = imageUrls.map((imageurl: string) => {
+            return {
+                src: imageurl,
+                original: imageurl,
+                width: 420,
+                height: 283,
+                caption: "37H (gratispgraphy.com)",
+            };
+        });
+
+        const slides = imgs.map(({ src, width, height }) => ({
+            src: src,
+            width: 800,
+            height: 800,
+        }));
+        console.log(imgs);
+        console.log(slides);
+        return {
+            props: {
+                imgs,
+                slides,
+            },
+        };
+    } else {
+        return {
+            props: {
+                imgs: [],
+                slides: []
+            },
+        };
+    }
+}
