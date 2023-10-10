c4cbf98df3ceacc1096a9a5270714a9d837519fc
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-10-10 05:36:02 +0530

    server fetching code done

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import PageLayout from "@/components/layout/PageLayout";
 import styles from "@/styles/Schedule/schedule.module.css";
+import { GetServerSidePropsContext } from "next";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/serverless/firebase";
+import { Event } from "@/types/Event";
 
-function Schedule() {
+interface Schedule {
+    day1: Event[],
+    day2: Event[],
+    day3: Event[]
+}
+interface Props {
+    schedule: Schedule
+}
+
+function Schedule({schedule}: Props) {
+
     return (
         <PageLayout title="Schedule | PECFEST'23">
-            <main className={styles.main}>AGENDA FORMAT COMING UP</main>
+            <main className={styles.main}>
+                <div className={styles.schedule}></div>
+            </main>
         </PageLayout>
     );
 }
-
 export default Schedule;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const colRef = collection(db, "events");
+    const events = await getDocs(colRef);
+
+    let schedule = {
+        day1: [],
+        day2: [],
+        day3: [],
+    } as Schedule;
+
+    events.docs.map((doc) => {
+        const date = new Date(Date.parse(doc.data().startDate));
+        const day1 = new Date(Date.parse("2023-11-03"));
+        const day2 = new Date(Date.parse("2023-11-04"));
+        const day3 = new Date(Date.parse("2023-11-05"));
+
+        if (date.getDay() == day1.getDay()) {
+            schedule.day1.push(doc.data() as Event);
+        } else if (date.getDay() == day2.getDay()) {
+            schedule.day2.push(doc.data() as Event);
+        } else if (date.getDay() == day3.getDay()) {
+            schedule.day3.push(doc.data() as Event);
+        }
+    });
+
+    return {
+        props: {
+            schedule,
+        },
+    };
+}
